bf25016adff22dc208435cdb4ba707170f41195c
t.ozawa <t.ozawa1230@gmail.com>
2021-09-07 12:55:25 +0900

    test for resize window

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ const { windowManager } = require("node-window-manager");
 const applescript = require("applescript");
 const fs = require("fs");
 const constants = require("./constants");
+const script = fs.readFileSync("./monitorSize.scpt", {encoding: "utf-8"});
 
 // This method has to be called on macOS before changing the window's bounds, otherwise it will throw an error.
 // It will prompt an accessibility permission request dialog, if needed.
 windowManager.requestAccessibility();
-const windows = windowManager.getWindows();
 
-windows.forEach((window)=> {
-    console.log(window.getTitle());
-    window.setBounds({ x: 0, y: 0 });
-});
+const getMonitorSize = () => {
+    return new Promise((resolve)=>{
+        applescript.execString(script, (err, output) => {
+            resolve(output)
+        });
+    })
+}
 
-const script = fs.readFileSync("./monitorSize.scpt", {encoding: "utf-8"});
 
-applescript.execString(script, (err, rtn) => {
-    console.log(rtn)
-});
+(async () => {
+    const monitorSize = await getMonitorSize();
+    const [_, __, width, height] = monitorSize;
+    const windows = windowManager.getWindows();
+    
+    windows.forEach((window)=> {
+        console.log(window.getTitle());
+        window.setBounds({ x: 0, y: 0, width, height });
+    });
+})();
