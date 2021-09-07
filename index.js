1117bbd69ff0a97cb97d31c359a5d13980ab4f39
t.ozawa <t.ozawa1230@gmail.com>
2021-09-07 11:53:28 +0900

    get window size by AppleScript

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const { windowManager } = require("node-window-manager");
+const applescript = require("applescript");
+const fs = require("fs");
 const constants = require("./constants");
-console.log(constants)
+
 // This method has to be called on macOS before changing the window's bounds, otherwise it will throw an error.
 // It will prompt an accessibility permission request dialog, if needed.
 windowManager.requestAccessibility();
-
 const windows = windowManager.getWindows();
 
 windows.forEach((window)=> {
     console.log(window.getTitle());
     window.setBounds({ x: 0, y: 0 });
-})
+});
 
-// Prints the currently focused window bounds.
+const script = fs.readFileSync("./monitorSize.scpt", {encoding: "utf-8"});
 
-// Sets the active window's bounds.
+applescript.execString(script, (err, rtn) => {
+    console.log(rtn)
+});
---
+++ monitorSize.scpt
@@ -0,0 +1,4 @@
+tell application "Finder"
+        bounds of the window of the desktop
+end tell
+set {x1, y1, x2, y2} to result
