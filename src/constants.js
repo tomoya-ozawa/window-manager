3a2ebd839f987516d9fc1cc5f58c663f7004160e
Tomoya Ozawa <s.tomoya.ozawa@smartdrive.co.jp>
2021-09-08 10:39:27 +0900

    add new apps

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,30 @@
 const CONSTANTS = {
-    apps: {
-        chrome: {   
-            alias: "ch",
-            windowTitle: "Google Chrome"
-        },
-        vscode: {
-            alias: "vi",
-            windowTitle: "Code"
-        },
-        iterm: {
-            alias: "ter",
-            windowTitle: "iTerm"
-        },
-    }
+  apps: {
+    chrome: {
+      alias: "ch",
+      windowTitle: "Google Chrome",
+    },
+    vscode: {
+      alias: "vi",
+      windowTitle: "Code",
+    },
+    iterm: {
+      alias: "ter",
+      windowTitle: "iTerm",
+    },
+    figma: {
+      alias: "fig",
+      windowTitle: "Figma",
+    },
+    notion: {
+      alias: "not",
+      windowTitle: "Notion",
+    },
+    slack: {
+      alias: "sl",
+      windowTitle: "Slack",
+    },
+  },
 };
 
 module.exports = CONSTANTS;
